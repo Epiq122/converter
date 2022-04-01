d48492aa7ca970f1731cb8ccab33e39702416549
Epiq122 <l2ob122@gmail.com>
2022-03-31 21:28:21 -0700

    added the logic for mass and volume

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const lengthEl = document.getElementById('length');
-let unitEl = document.getElementById('unit');
-
-unitEl.textContent = 20;
+const unitEl = document.getElementById('unit');
+const volumeEl = document.getElementById('volume');
+const massEl = document.getElementById('mass');
+let numberToConvert = 20;
+unitEl.textContent = numberToConvert;
 
 function getLength() {
-  unitEl = 20;
-  const metersToFeet = `${Number(3.28084 * unitEl).toFixed(2)} feet`;
-  const feetTometers = `${Number(unitEl / 3.281).toFixed(2)} meters`;
+  numberToConvert = 20;
+  const metersToFeet = `${Number(3.28084 * numberToConvert).toFixed(2)} feet`;
+  const feetTometers = `${Number(numberToConvert / 3.281).toFixed(2)} meters`;
 
-  lengthEl.textContent = `${unitEl} meters = ${metersToFeet} | ${unitEl} feet = ${feetTometers}`;
+  lengthEl.textContent = `${numberToConvert} meters = ${metersToFeet} | ${numberToConvert} feet = ${feetTometers}`;
 }
+
+function getVolume() {
+  numberToConvert = 20;
+  const literToGallon = `${Number(numberToConvert / 3.785).toFixed(2)} gallons`;
+  const gallonsToLiters = `${Number(3.785 * numberToConvert).toFixed(
+    2
+  )} liters`;
+  volumeEl.textContent = `${numberToConvert} liters = ${literToGallon} | ${numberToConvert} gallons = ${gallonsToLiters}`;
+}
+
+function getMass() {
+  numberToConvert = 20;
+  const kilosToPounds = `${Number(numberToConvert * 2.205).toFixed(2)}`;
+  const poundsToKilos = `${Number(numberToConvert / 2.205).toFixed(2)}`;
+  massEl.textContent = `${numberToConvert} kilos = ${kilosToPounds} | ${numberToConvert} pounds = ${poundsToKilos}`;
+}
+
 getLength();
+getVolume();
+getMass();
